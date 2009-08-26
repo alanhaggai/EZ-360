6a912ef10e902728fb81f776fe45b83251a98a59
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2009-08-26 11:31:27 +0530

    Model Article

---
+++ lib/EZ/360/Schema.pm
@@ -0,0 +1,16 @@
+package EZ::360::Schema;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2009-08-26 11:15:43
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:JAn2Iw4z42Qn0RjyGAwekQ
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
---
+++ lib/EZ/360/Schema/Result/Article.pm
@@ -0,0 +1,66 @@
+package EZ::360::Schema::Result::Article;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "Core");
+__PACKAGE__->table("article");
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type => "INTEGER",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
+  "title",
+  {
+    data_type => "TEXT",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
+  "content",
+  {
+    data_type => "TEXT",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
+  "created",
+  {
+    data_type => "INTEGER",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
+  "updated",
+  {
+    data_type => "INTEGER",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
+);
+__PACKAGE__->set_primary_key("id");
+
+
+# Created by DBIx::Class::Schema::Loader v0.04006 @ 2009-08-26 11:15:43
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:tXpv92UjkS4muY6wMcu2xA
+
+__PACKAGE__->add_columns(
+    created => {
+        data_type     => 'datetime',
+        set_on_create => 1,
+    },
+    updated => {
+        data_type     => 'datetime',
+        set_on_create => 1,
+        set_on_update => 1,
+    },
+);
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
