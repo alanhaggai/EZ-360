e266b07f12933baf5136c8bfc28fa82966df554e
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2009-08-26 16:43:09 +0530

    WIP: Create User do

--- lib/EZ/360/Controller/User.pm
+++ lib/EZ/360/Controller/User.pm
@@ -22,6 +22,60 @@ sub create : Local : Args(0) {
     $c->stash( template => 'user/create.html' );
 }
 
+sub create_do : Path('create/do') : Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $username           = $c->request->body_params->{'username'};
+    my $password           = $c->request->body_params->{'password'};
+    my $email              = $c->request->body_params->{'email'};
+    my $realname           = $c->request->body_params->{'realname'};
+    my $can_create_article = $c->request->body_params->{'can-create-article'};
+    my $can_update_article = $c->request->body_params->{'can-update-article'};
+    my $can_delete_article = $c->request->body_params->{'can-delete-article'};
+    my $can_create_user    = $c->request->body_params->{'can-create-user'};
+    my $can_update_user    = $c->request->body_params->{'can-update-user'};
+    my $can_delete_user    = $c->request->body_params->{'can-delete-user'};
+    my $superuser          = $c->request->body_params->{'superuser'};
+
+    if ( $username && $password && $email ) {
+        my $user;
+        eval {
+            $user = $c->model('DB::User')->create(
+                {
+                    username => $username,
+                    password => $password,
+                    email    => $email,
+                    realname => $realname,
+                }
+            );
+        };
+
+        unless ($@) {
+            $c->response->redirect(
+                $c->uri_for(
+                    '/user/' . $user->id() . '/retrieve',
+                    { status_message => 'User created successfully.' }
+                )
+            );
+
+            return;
+        }
+    }
+
+    $c->response->redirect(
+        $c->uri_for(
+            '/error',
+            { error_message => 'Error occurred while creating user.' }
+        )
+    );
+}
+
+sub id : Chained('/') : PathPart('user') : CaptureArgs(1) {
+    my ( $self, $c, $id ) = @_;
+
+    $c->stash( user => $c->model('DB::User')->find($id) );
+}
+
 =head1 AUTHOR
 
 Alan Haggai Alavi
