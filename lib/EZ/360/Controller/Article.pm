c22970160a96cad7e75ff9f3a707221dee0ccb99
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2009-08-26 15:34:37 +0530

    Update Article
    Both Create Article and Update Article fail with error message when null
    title/content is provided

--- lib/EZ/360/Controller/Article.pm
+++ lib/EZ/360/Controller/Article.pm
@@ -18,6 +18,7 @@ Catalyst Controller.
 
 sub create : Local : Args(0) {
     my ( $self, $c ) = @_;
+
     $c->stash( template => 'article/create.html' );
 }
 
@@ -38,26 +39,27 @@ sub create_do : Path('create/do') : Args(0) {
             );
         };
 
-        if ($@) {
-            $c->response->redirect(
-                $c->uri_for(
-                    '/error',
-                    {
-                        error_message =>
-                          'Error occurred while creating article.'
-                    }
-                )
-            );
-        }
-        else {
+        unless ($@) {
             $c->response->redirect(
                 $c->uri_for(
                     '/article/' . $article->id() . '/retrieve',
                     { status_message => 'Article created successfully.' }
                 )
             );
+
+            return;
         }
     }
+
+    $c->response->redirect(
+        $c->uri_for(
+            '/error',
+            {
+                error_message =>
+                  'Error occurred while creating article.'
+            }
+        )
+    );
 }
 
 sub id : Chained('/') : PathPart('article') : CaptureArgs(1) {
@@ -76,10 +78,45 @@ sub delete_do : Chained('id') : PathPart('delete/do') : Args(0) {
 
 sub update : Chained('id') : PathPart('update') : Args(0) {
     my ( $self, $c ) = @_;
+
+    $c->stash( template => 'article/update.html' );
 }
 
 sub update_do : Chained('id') : PathPart('update/do') : Args(0) {
     my ( $self, $c ) = @_;
+
+    my $title   = $c->request->body_params->{'title'};
+    my $content = $c->request->body_params->{'content'};
+
+    if ( $title && $content ) {
+        my $article = $c->stash->{article};
+        eval {
+            $article->title($title);
+            $article->content($content);
+            $article->update();
+        };
+
+        unless ($@) {
+            $c->response->redirect(
+                $c->uri_for(
+                    '/article/' . $article->id() . '/retrieve',
+                    { status_message => 'Article updated successfully.' }
+                )
+            );
+
+            return;
+        }
+    }
+
+    $c->response->redirect(
+        $c->uri_for(
+            '/error',
+            {
+                error_message =>
+                  'Error occurred while updating article.'
+            }
+        )
+    );
 }
 
 sub retrieve : Chained('id') : PathPart('retrieve') : Args(0) {
