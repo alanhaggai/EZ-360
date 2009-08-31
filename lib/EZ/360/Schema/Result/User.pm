f08106a01699ae8009319b06ce83d63d74ea9896
Alan Haggai Alavi <alanhaggai@alanhaggai.org>
2009-08-31 21:55:34 +0530

    Gravatar and setting roles moved to model

--- lib/EZ/360/Schema/Result/User.pm
+++ lib/EZ/360/Schema/Result/User.pm
@@ -62,16 +62,38 @@ __PACKAGE__->add_columns(
             salt_length => 10,
         },
         encode_check_method => 'check_password',
-      },
+    },
 );
 
 __PACKAGE__->has_many(
-    user_role => 'EZ::360::Schema::Result::UserRole', 'user_id'
+    user_role => 'EZ::360::Schema::Result::UserRole',
+    'user_id'
 );
 
 __PACKAGE__->many_to_many(
-    roles => 'user_role', 'role'
+    roles => 'user_role',
+    'role'
 );
+
+sub set_all_roles {
+    my ( $self, $roles ) = @_;
+
+    $self->user_role->delete();
+    for ( @{$roles} ) {
+        $self->user_role->create( { role_id => $_ } );
+    }
+}
+
+sub gravatar {
+    my ( $self, $size ) = @_;
+
+    use Gravatar::URL ('gravatar_url');
+    return gravatar_url(
+        email   => $self->email,
+        size    => $size,
+        default => 'identicon'
+    );
+}
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 1;
